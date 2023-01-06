6410582ee30b1901b33b07d301f50c3d076b3f99
一月君言 <tengpeng.gao@gmail.com>
2023-01-06 19:15:16 +0800

    update apollo

---
+++ sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/rule/apollo/ApolloConfig.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 1999-2018 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.alibaba.csp.sentinel.dashboard.rule.apollo;
+
+import com.alibaba.csp.sentinel.dashboard.datasource.entity.rule.FlowRuleEntity;
+import com.alibaba.csp.sentinel.datasource.Converter;
+import com.alibaba.fastjson.JSON;
+import com.ctrip.framework.apollo.openapi.client.ApolloOpenApiClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+/**
+ * @author hantianwei@gmail.com
+ * @since 1.5.0
+ */
+@Configuration
+public class ApolloConfig {
+
+    @Bean
+    public Converter<List<FlowRuleEntity>, String> flowRuleEntityEncoder() {
+        return JSON::toJSONString;
+    }
+
+    @Bean
+    public Converter<String, List<FlowRuleEntity>> flowRuleEntityDecoder() {
+        return s -> JSON.parseArray(s, FlowRuleEntity.class);
+    }
+
+    /**
+     *  // sentinel-test
+     *  // 根据实际情况修改
+     *  // apollo开发api的token
+     * @return
+     */
+    @Bean
+    public ApolloOpenApiClient apolloOpenApiClient() {
+        ApolloOpenApiClient client = ApolloOpenApiClient.newBuilder()
+                .withPortalUrl("http://10.22.84.217:18070")
+                .withToken("b4bad40484231b6fc213e31a948cc3100fe6b85b")
+                .build();
+        return client;
+
+    }
+}
